dd16ea3f6f8d803e4dde4c263ed601285f7d418e
rupertsky <cajas25@hotmail.com>
2022-09-08 17:34:02 -0500

    obj comm

--- NYC_G3-app.py
+++ NYC_G3-app.py
@@ -28,7 +28,7 @@ c1.write('La siniestralidad vial hace referencia al conjunto de eventos trágico
          'humanos los cuales se podrían haber evitado. En la ciudad de Nueva York, una de las ciudades más grandes del '
          'mundo con aproximadamente 8.38 millones de habitantes para el 2020, diariamente se registra un gran numero '
          'de siniestros viales. '
-         'Como principal motivo de este proyecto, se buscan proponer alternativas para la '
+         'Como principal objetivo de este proyecto, se buscan proponer alternativas para la '
          'movilidad vehicular, que permitan reducir la siniestralidad vial de la ciudad de Nueva York')
 c1.image(image2, caption='Accidente', width=350)
 
