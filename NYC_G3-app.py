85e58a681369fbfe5924fe1d5656b8c98174484a
rupertsky <cajas25@hotmail.com>
2022-09-08 16:48:19 -0500

    streamlit commit

--- NYC_G3-app.py
+++ NYC_G3-app.py
@@ -5,8 +5,15 @@ image = Image.open('Docs/DW_Pipeline.png')
 # LINKS
 main_repo = 'https://github.com/rupertsky/ProyectoGrupalNYC'
 etl = 'https://youtu.be/V90fLjwq7KQ'
+docs = 'https://github.com/rupertsky/ProyectoGrupalNYC/tree/main/Docs'
 #
 st.title('Análisis de accidentalidad en la ciudad de New York')
+
+st.markdown('**Integrantes**')
+st.caption('_Carlos Gaviria_')
+st.caption('_Jean Carlos Betancourt_')
+st.caption('_Alexander Imbachi_')
+st.caption('_Juan Diego Gutierrez_')
 
 st.header('Overview del proyecto')
 st.subheader('_Vista General_')
@@ -14,24 +21,36 @@ c1 = st.container()
 c1.write('La siniestralidad vial hace referencia al conjunto de eventos trágicos causados generalmente por errores '
          'humanos los cuales se podrían haber evitado. En la ciudad de Nueva York, una de las ciudades más grandes del '
          'mundo con aproximadamente 8.38 millones de habitantes para el 2020, diariamente se registra un gran numero '
-         'de siniestros viales. \n'
+         'de siniestros viales. '
          'Como principal motivo de este proyecto, se buscan proponer alternativas para la '
          'movilidad vehicular, que permitan reducir la siniestralidad vial de la ciudad de Nueva York')
 
-st.subheader('_Solucion Propuesta_')
+st.subheader('_Solución Propuesta_')
 c2 = st.container()
 c2.write('Para llevar a cabo el proyecto, se ideo un alcance en el que se especifican todas las '
-         'caracteristicas que este posee y de esta forma cumplir con el objetivo estrategico.')
+         'características que este posee y de esta forma cumplir con el objetivo estratégico.')
 c2.caption('_Vista al Proyecto: [GitHub](%s)_' % main_repo)
 
 st.header('ETL del Proyecto')
-st.subheader('_Extraccion, Transformacion, Carga de Datos_')
+st.subheader('_Extracción, Transformación, Carga de Datos_')
 c3 = st.container()
-c3.write('El proceso de ETL tiene como principales actores el lenguaje de programacion Python, '
+c3.write('El proceso de ETL tiene como principales actores el lenguaje de programación Python, '
          'el framework Apache Spark, Amazon AWS como encargado de carga incremental, almacenamiento en la nube (RDS) y '
          'finalmente MySQL como motor SQL. El mismo esta programado para ejecutarse todos los dias a las 19:00 GMT-5.')
 c3.image(image, caption='DW Pipeline', width=500)
 c3.write('Por medio de un video explicativo se demuestra el paso a paso del proceso.')
 c3.caption('_Vista ETL: [Ir al Video](%s)_' % etl)
 
+st.header('Entregables')
+st.subheader('_Dashboard, Documentación_')
+c4 = st.container()
+c4.write('Power BI, es una herramienta de visualización y análisis de datos enfocada a la productividad empresarial, '
+         'por este motivo, se implemento un Dashboard en el que se evidencian diferentes tipos de siniestralidad, '
+         'como por ejemplo accidentes por calle, tipo, trafico, etc. De esta forma se pueden tomar decisiones '
+         'que disminuyan aquellas estadísticas con el fin de mejorar la calidad vial en la ciudad de New York.')
+c4.write('Adicionalmente, se hace entrega del respectivo Manual de Usuario para el correcto manejo del dashboard y '
+         'de esta forma, aprovechar al máximo sus capacidades.')
+c4.caption('Para acceder a Documentos/Entregables ingrese aquí: [Docs](%s)_' % docs)
+
+st.caption('_**HENRY**_')
 st.markdown("<a href='#an-lisis-de-accidentalidad-en-la-ciudad-de-new-york'>Ir Arriba ^</a>", unsafe_allow_html=True)
